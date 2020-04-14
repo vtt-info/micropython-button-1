cf4196145a933592e55cf2e39db79260104ef76c
Jan Češpivo <jan.cespivo@gmail.com>
2020-04-14 20:42:12 +0200

    Implementation

--- button.py
+++ button.py
@@ -3,23 +3,30 @@ from micropython import schedule
 
 
 class Button:
-    debounce_time_ms = 20
+    debounce_time_ms = 10
 
-    def __init__(self, pin_num: int, up_state=0):
-        self._callbacks = []
-        self._pin = Pin(pin_num, Pin.IN)
-        self._pin.irq(trigger=Pin.IRQ_FALLING, handler=self._on_press)
+    def __init__(self, pin_num: int, release_value=0):
+        self._on_press_callbacks = []
+        self._on_release_callbacks = []
+        self.pin = Pin(pin_num, Pin.IN, Pin.PULL_DOWN)  # TODO pull
         self._debounce_timer = Timer(1)
-        self._up_state = up_state
+        self._value = release_value
+        self._release_value = release_value
+        self._register_irq()
 
-    def _on_press(self, pin):
+    def _register_irq(self):
+        self.pin.irq(
+            trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING,
+            handler=self._irq,
+        )
+
+    def _irq(self, pin):
         value = pin.value()
-        if value == self._up_state:
+        if value == self._value:
             return
 
-        self._value = value
         pin.irq(trigger=0)
-
+        self._value = value
         self._debounce_timer.init(
             period=Button.debounce_time_ms,
             mode=Timer.ONE_SHOT,
@@ -27,13 +34,26 @@ class Button:
         )
 
     def _debounce(self, _):
-        self._pin.irq(trigger=Pin.IRQ_FALLING, handler=self._on_press)
-        if self._pin.value() == self._value:
-            schedule(self._callback, None)
+        if self.pin.value() == self._value:
+            if self._value == self._release_value:
+                callback = self._on_release_callback
+            else:
+                callback = self._on_press_callback
+            schedule(callback, None)
+        self._register_irq()
 
-    def _callback(self, _):
-        for callback in self._callbacks:
+    def _on_press_callback(self, _):
+        for callback in self._on_press_callbacks:
             callback()
 
-    def register(self, callback):
-        self._callbacks.append(callback)
+    def _on_release_callback(self, _):
+        for callback in self._on_release_callbacks:
+            callback()
+
+    def on_press(self, callback):
+        self._on_press_callbacks.append(callback)
+        return callback
+
+    def on_release(self, callback):
+        self._on_release_callbacks.append(callback)
+        return callback
